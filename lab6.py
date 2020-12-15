085fb231578ad7eaa1a172d9bb0baa810658d209
Gomezjose47 <gomezjose47@mail.fresnostate.edu>
2020-12-14 18:27:50 -0800

    Endlab6

--- lab6.py
+++ lab6.py
@@ -37,4 +37,62 @@ loop three times
 
 """
 
+import random 
 
+total_earned = 0
+
+def main():
+  results = [1]
+  for i in range (0,3):
+    results.append(spin())
+
+  print ("results", results)
+  winner = jackpot(results)
+
+  if(winner):
+   print("Winner! you win")
+  else:
+    print("sorry try again")
+
+  total = count(results)
+  option = input("play again? ")
+  if option.lower() == "y" or option.lower == "yes" :
+
+def spin()
+ rand_num = random.randint(1, 20)
+  output = ""
+  if (rand_num > 15): 
+    output = "Cherries"
+  elif(rand_num > 10):
+    output = "Orange"
+  elif(rand_num > 5):
+    output = "Plum"
+  elif(rand_num > 2):
+    output = "Melon"
+  elif(rand_num > 1):
+    output = "Bell"
+  else:
+    output = "Bar"
+
+  print (output, end= "")
+
+def jackpot(results):
+  return results[0] == results [1] == results [2]
+
+def count(results):
+  total = 0
+  money_dict = {
+    "Cherries": 1,
+    "Oranges": .7,
+    "Plum": .6,
+    "Bell": .4,
+    "Melon": .2,
+    "Bar": .1
+  }
+
+  for i in results:
+    total += money_dict[1]
+  print("total)", total)
+  return total_earned += total
+
+main()
